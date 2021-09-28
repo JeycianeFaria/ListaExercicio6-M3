76b4ec174c85529ccf321dbf691e86d0478dd51e
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-09-28 06:41:09 -0300

    Modelagem da classe Imobiliaria

--- src/br/com/zup/Imobiliaria.java
+++ src/br/com/zup/Imobiliaria.java
@@ -1,4 +1,47 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imobiliaria {
+
+    //Atributos
+    private String nomeImobiliaria;
+    private List<Imovel> catalogoImoveis = new ArrayList<>();
+    private List<Funcionario> listaFuncionarios = new ArrayList<>();
+
+
+    //construtor
+    public Imobiliaria() {
+    }
+
+    public Imobiliaria(String nomeImobiliaria) {
+        this.nomeImobiliaria = nomeImobiliaria;
+    }
+
+
+    //Getters e Setters
+    public String getNomeImobiliaria() {
+        return nomeImobiliaria;
+    }
+
+    public void setNomeImobiliaria(String nomeImobiliaria) {
+        this.nomeImobiliaria = nomeImobiliaria;
+    }
+
+    public List<Imovel> getCatalogoImoveis() {
+        return catalogoImoveis;
+    }
+
+    public void setCatalogoImoveis(List<Imovel> catalogoImoveis) {
+        this.catalogoImoveis = catalogoImoveis;
+    }
+
+    public List<Funcionario> getListaFuncionarios() {
+        return listaFuncionarios;
+    }
+
+    public void setListaFuncionarios(List<Funcionario> listaFuncionarios) {
+        this.listaFuncionarios = listaFuncionarios;
+    }
 }
